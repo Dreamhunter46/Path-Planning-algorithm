81f989efd63acc2b5ba11b6136045631602e2b0a
Dreamhunter46 <62807485+Dreamhunter46@users.noreply.github.com>
2021-09-29 16:47:46 +0200

    Add files via upload

---
+++ RRT.py
@@ -0,0 +1,67 @@
+
+import pygame
+from RRTbase import RRTGraph
+from RRTbase import RRTMap
+import time
+
+def main():
+    dimensions =(600,600)
+    start=(50,50)
+    goal=(500,500)
+    obsdim=30
+    obsnum=50
+    iteration=0
+    t1=0
+
+    pygame.init()
+    map=RRTMap(start,goal,dimensions,obsdim,obsnum)
+    graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
+
+    obstacles=graph.makeobs()
+    map.drawMap(obstacles)
+    smooth = []
+    t1=time.time()
+    while (not graph.path_to_goal()):
+        time.sleep(0.005)
+        elapsed=time.time()-t1
+        t1=time.time()
+        #raise exception if timeout
+        if elapsed > 100:
+            print('timeout re-initiating the calculations')
+            pygame.display.update()
+            pygame.event.clear()
+            pygame.event.wait(0)
+
+            raise
+
+        if iteration % 10 == 0:
+            X, Y, Parent = graph.bias(goal)
+            pygame.draw.circle(map.map, map.grey, (X[-1], Y[-1]), map.nodeRad*2, 0)
+            pygame.draw.line(map.map, map.Blue, (X[-1], Y[-1]), (X[Parent[-1]], Y[Parent[-1]]),
+                             map.edgeThickness)
+
+        else:
+            X, Y, Parent = graph.expand()
+            pygame.draw.circle(map.map, map.grey, (X[-1], Y[-1]), map.nodeRad*2, 0)
+            pygame.draw.line(map.map, map.Blue, (X[-1], Y[-1]), (X[Parent[-1]], Y[Parent[-1]]),
+                             map.edgeThickness)
+
+        if iteration % 5 == 0:
+            pygame.display.update()
+        iteration += 1
+    map.drawPath(graph.waypoints2path())
+
+    pygame.display.update()
+    pygame.event.clear()
+    pygame.event.wait(0)
+
+
+
+if __name__ == '__main__':
+    result=False
+    while not result:
+        try:
+            main()
+            result=True
+        except:
+            result=False
